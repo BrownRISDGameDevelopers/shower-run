45a902d02923708ee234bcb0e0d71c852cb156ad
LucianCravens <lcravens@risd.edu>
2023-10-07 16:05:37 -0400

    Hiding

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -12,10 +12,15 @@ public class PlayerInteraction : MonoBehaviour
     [Header("Player Input")]
     [SerializeField] KeyCode interactKey;
 
+    //Temp location for the variable
+    bool isHiding = false;
+
     // Update is called once per frame
     void Update()
     {
         CheckRaycast();
+
+        CheckLeaveHiding();
     }
 
     void CheckRaycast()
@@ -24,20 +29,56 @@ public class PlayerInteraction : MonoBehaviour
 
         if (Physics.Raycast(raycastPosition.position, transform.forward, out hit, interactRange, interactLayer))
         {
-            LookingAtInteractableObject(hit);
+            GameObject interactableObject = hit.collider.gameObject;
+            LookingAtInteractableObject(interactableObject);
         }
     }
 
-    void LookingAtInteractableObject(RaycastHit hit)
+    void LookingAtInteractableObject(GameObject obj)
     {
         if(Input.GetKeyDown(interactKey))
         {
-            InteractedWithObject();
+            InteractedWithObject(obj);
         }
     }
 
-    void InteractedWithObject()
+    void InteractedWithObject(GameObject obj)
     {
         Debug.Log("Object was interacted with");
+        StartCoroutine(TeleportPlayerIntoObject(obj));
+    }
+
+    IEnumerator TeleportPlayerIntoObject(GameObject obj)
+    {
+        transform.parent = obj.transform;
+        transform.localPosition = Vector3.zero;
+
+        //1 frame delay so no conflict with leaving hiding
+        yield return null;
+
+        isHiding = true;
+    }
+
+    void CheckLeaveHiding()
+    {
+        if(isHiding && Input.GetKeyDown(interactKey)) //Also check if player is in hiding
+        {
+            StartCoroutine(LeaveHiding());
+        }
+    }
+
+    IEnumerator LeaveHiding()
+    {
+        //Getting the step out point
+        Transform stepOutPoint = transform.parent.transform.Find("Player Step Out Point");
+
+        //Setting player's position to the hiding step out point
+        transform.position = stepOutPoint.position;
+
+        transform.parent = null;
+
+        yield return null;
+
+        isHiding = false;
     }
 }
