2f4ecea5da6e937a62d774c7f7d8385012034d4f
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2023-10-07 16:11:42 -0400

    integrate new input system into player interaction

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -7,23 +7,23 @@ public class PlayerInteraction : MonoBehaviour
 {
     [Header("Raycasting")]
     [SerializeField] private float interactRange = 2.5f;
-    [FormerlySerializedAs("raycastPosition")] [SerializeField] private Transform raycastOrigin; //Where the raycast is shot from on the player's body
+    [SerializeField] private Transform raycastOrigin; 
     
     private LayerMask _interactLayer; 
 
     void Start()
     { 
-        _interactLayer = LayerMask.NameToLayer("Interactable"); 
+        _interactLayer = ~LayerMask.NameToLayer("Interactable"); 
     }
     
     public void OnInteract()
     {
         Debug.Log("Interact");
         RaycastHit hit;
-        Debug.DrawRay(raycastOrigin.position, raycastOrigin.TransformDirection(Vector3.forward) * interactRange);
-        if (Physics.Raycast(raycastOrigin.position, raycastOrigin.TransformDirection(Vector3.forward), out hit, interactRange, _interactLayer))
+        Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward * 1000, Color.green, 6);
+        if (Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hit, interactRange, _interactLayer))
         {
-            Debug.Log("Object was interacted with");
+            Debug.Log("Interacted with object");
         }
     }
 }
