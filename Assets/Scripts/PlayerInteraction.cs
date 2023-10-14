6e2b48746d84e87a3678ad504b1e1db9f778ebb4
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2023-10-14 14:43:27 -0400

    implement rudimentary hiding and exiting behavior

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -10,18 +10,45 @@ public class PlayerInteraction : MonoBehaviour
     [SerializeField] private Transform raycastOrigin; 
     
     private LayerMask _interactLayer; 
+    private Rigidbody _playerBody;
+    private bool _isHiding = false;
 
     void Start()
     { 
         _interactLayer = ~LayerMask.NameToLayer("Interactable"); 
     }
+
+    void Awake()
+    {
+        _playerBody = gameObject.GetComponent<Rigidbody>();
+    }
     
     public void OnInteract()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hit, interactRange, _interactLayer))
+        if (_isHiding)
         {
-            Debug.Log("Interacted with object");
+            ExitHiding();
         }
+        else if (Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out var hit, interactRange, _interactLayer))
+        {
+           EnterHiding(hit.transform); 
+        }
+    }
+
+    private void EnterHiding(Transform other)
+    {
+            _playerBody.isKinematic = true;
+            _playerBody.velocity = Vector3.zero;
+            var o = gameObject.transform;
+            o.position = other.position;
+            o.rotation = other.rotation;
+            // TODO: limit rotation when hiding
+    }
+
+    private void ExitHiding()
+    {
+        _playerBody.isKinematic = false;
+        // TODO: Need to change this to follow the behavior talked about with producers
+        gameObject.transform.position += gameObject.transform.forward;
     }
 }
