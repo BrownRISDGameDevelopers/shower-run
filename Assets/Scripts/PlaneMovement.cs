8a181ce814ff0e92b6809faa7eefdf06bc645e51
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2023-10-28 14:27:46 -0400

    add plane behavior

--- Assets/Scripts/PlaneMovement.cs
+++ Assets/Scripts/PlaneMovement.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 
 public class PlaneMovement : MonoBehaviour
 {
-    [SerializeField] private float trailingDistance;
+    /// <summary>
+    /// The DeathPlane's movement behavior.
+    /// There's two rules to its behavior, in descending priority
+    /// 1. The DeathPlane will at most be <c>maxTrailingDistance</c> behind the player
+    /// 2. The DeathPlane move at speed <c>minSpeed</c>
+    /// </summary>
+    [SerializeField] private float maxTrailingDistance;
     [SerializeField] private float minSpeed;
     [SerializeField] private Rigidbody player;
     
     void Update()
     {
-        gameObject.transform.position += new Vector3(math.max(player.velocity.x * Time.deltaTime, minSpeed * Time.deltaTime), 0f, 0f);
+        Vector3 planePos = gameObject.transform.position;
+        
+        if (IsFarFromPlayer())
+        {
+            gameObject.transform.position =
+                new Vector3(player.position.x - maxTrailingDistance, planePos.y, planePos.z);
+        }
+        else
+        {
+            gameObject.transform.position += new Vector3(minSpeed * Time.deltaTime, 0f, 0f);
+        }
+    }
+
+    private bool IsFarFromPlayer()
+    {
+        return (player.position - gameObject.transform.position).x > maxTrailingDistance;
     }
 }
