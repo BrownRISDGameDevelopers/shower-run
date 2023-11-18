9602b44159e2d8d1dcf070011e423a65c25eacf1
Seong-Heon (Sunghun) Jung <castlehoneyjung@gmail.com>
2023-11-18 16:36:48 -0500

    debug move audio play

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _playerBody;
     private PlayerInputActions _actions;
     private AudioSource running;
-    private int status = 0;
+    private bool moveAudioStarted = false;
     
     void Awake()
     {
@@ -32,20 +32,28 @@ public class PlayerMovement : MonoBehaviour
         var moveDirection = new Vector3(inputDirection.x, 0f, inputDirection.y);
 
         float relativeSpeedMultiplier = limit - Mathf.Clamp(Mathf.Abs(Vector3.Dot(_playerBody.velocity, moveDirection.normalized)), 0f, 1f);
-        if(_playerBody.velocity.x != 0 && _playerBody.velocity.z != 0) {
 
-            if((Mathf.Abs(moveDirection.x) > 0f || Mathf.Abs(moveDirection.z) > 0f) && status == 0) {
-            print("hi");
-            running.Play();
-            status = 1;
-        } else if(Mathf.Abs(moveDirection.x) == 0f && Mathf.Abs(moveDirection.z) == 0f) {
-            print("bye");
+        bool receivedMoveInput = ReceiveMoveInput(moveDirection);
+
+        if (receivedMoveInput)
+        {
+            if (!moveAudioStarted)
+            {
+                running.Play();
+                moveAudioStarted = true;
+            }
+            _playerBody.AddRelativeForce(moveDirection.normalized * (multiplier * relativeSpeedMultiplier));
+        }
+        else
+        {
             running.Pause();
-            status = 0;
+            moveAudioStarted = false;
         }
-        }
-        _playerBody.AddRelativeForce(moveDirection.normalized * (multiplier * relativeSpeedMultiplier));
-        
+    }
+
+    private bool ReceiveMoveInput(Vector3 moveDirection)
+    {
+        return (Mathf.Abs(moveDirection.x) > 0f || Mathf.Abs(moveDirection.z) > 0f) && !GameManager.Instance.isHiding;
     }
 
     private void OnEnable() { _actions.gameplay.Enable(); }
